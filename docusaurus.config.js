dbcefd4b5dddd7a5735326419667b08ce7a751d7
Tomaz Wang <TomazWang.tw@gmail.com>
2022-02-11 19:09:03 +0800

    github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://tomazwang.github.io/',
+  baseUrl: '/keep.writing/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TomazWang.tw', // Usually your GitHub org/user name.
+  projectName: 'keep.writing', // Usually your repo name.
 
   presets: [
     [
